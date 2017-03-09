a80c4f9959417644b76ee404f949cb6da45f43a6
Kristofer Krause <kris.krause@gmail.com>
2017-03-08 19:11:51 -0500

    unit test init

---
+++ tests/encryptDecrypt.test.js
@@ -0,0 +1,18 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const passPhrase = "i like CryptoJs";
+
+describe("crypto", () => {
+    it("should encrypt test", () => {
+        let actual = true;
+
+        expect(actual).to.equal(true);
+    });
+
+    it("should decrypt test", () => {
+        let actual = true;
+
+        expect(actual).to.equal(true);
+    });
+});
