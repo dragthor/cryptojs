ee6e0c3d087b99d76e692eff5a8fee3b4760d504
Kristofer Krause <kris.krause@gmail.com>
2017-03-10 16:39:59 -0500

    copy to clipboard; ctrl + c

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const electron = require('electron');
-const { dialog } = require('electron');
+const electron = require("electron");
+const { dialog } = require("electron");
 
-const { Menu, MenuItem } = electron;
+const { Menu, MenuItem, clipboard } = electron;
 
-var fs = require('fs');
+var fs = require("fs");
 
 // Module to control application life.
 const app = electron.app;
+
+const ipc = electron.ipcMain;
 
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
@@ -14,19 +16,19 @@ const BrowserWindow = electron.BrowserWindow;
 // Menu
 const template = [
   {
-    label: 'File',
-    role: 'window',
+    label: "File",
+    role: "window",
     submenu: [
       {
-        label: 'Open',
-        accelerator: 'CmdOrCtrl+O',
+        label: "Open",
+        accelerator: "CmdOrCtrl+O",
         click() {
           dialog.showOpenDialog(function (fileNames) {
             if (typeof fileNames === "undefined") return;
 
             var file = fileNames[0];
 
-            fs.readFile(file, 'utf-8', function (err, data) {
+            fs.readFile(file, "utf-8", function (err, data) {
               if (err != null) console.log(err);
 
               mainWindow.webContents.executeJavaScript("document.getElementById('txtInput').value = '" + data.trim() + "';");
@@ -37,8 +39,8 @@ const template = [
         }
       },
       {
-        label: 'Save',
-        accelerator: 'CmdOrCtrl+S',
+        label: "Save",
+        accelerator: "CmdOrCtrl+S",
         click() {
           dialog.showSaveDialog(function (fileName) {
             if (typeof fileName === "undefined") return;
@@ -56,17 +58,17 @@ const template = [
         }
       },
       {
-        role: 'close'
+        role: "close"
       }
     ]
   },
 
   {
-    label: 'View',
+    label: "View",
     submenu: [
       {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
+        label: "Reload",
+        accelerator: "CmdOrCtrl+R",
         click(item, focusedWindow) {
           if (focusedWindow) focusedWindow.reload();
         }
@@ -75,18 +77,18 @@ const template = [
   },
 
   {
-    role: 'help',
+    role: "help",
     submenu: [
       {
-        label: 'Learn More',
-        accelerator: 'CmdOrCtrl+M',
-        click() { require('electron').shell.openExternal('https://github.com/dragthor/cryptojs'); }
+        label: "Learn More",
+        accelerator: "CmdOrCtrl+M",
+        click() { require("electron").shell.openExternal("https://github.com/dragthor/cryptojs"); }
       },
 
       {
-        label: 'Random Password',
-        accelerator: 'CmdOrCtrl+P',
-        click() { require('electron').shell.openExternal('https://www.random.org/passwords/?num=5&len=12&format=html&rnd=new'); }
+        label: "Random Password",
+        accelerator: "CmdOrCtrl+P",
+        click() { require("electron").shell.openExternal("https://www.random.org/passwords/?num=5&len=12&format=html&rnd=new"); }
       },
     ]
   },
@@ -95,7 +97,7 @@ const template = [
 
 const menu = Menu.buildFromTemplate(template);
 
-// Keep a global reference of the window object, if you don't, the window will
+// Keep a global reference of the window object, if you don"t, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
@@ -106,45 +108,49 @@ function createWindow() {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 
 
   Menu.setApplicationMenu(menu);
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
+app.on("activate", function () {
+  // On OS X it"s common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-// In this file you can include the rest of your app's specific main process
+// In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+ipc.on("control+C", (event, arg) => {
+  if (arg.length === 0) return;
 
+  clipboard.writeText(arg);
+});
--- renderer.js
+++ renderer.js
@@ -1,3 +1,35 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const electron = require("electron");
+const ipc = electron.ipcRenderer;
+
+var last = "";
+
+document.getElementById("body").addEventListener("keydown", function (e) {
+    if (last.length === 0) {
+        last = e.key;
+    } else {
+        if (last === "Control" && e.key === "c") {
+            last = "";
+
+            var selection = getSelectionText();
+
+            if (selection != null && selection.length > 0) {
+                ipc.send("control+C", selection);
+            }
+        } else {
+            last = e.key;
+        }
+    }
+});
+
+function getSelectionText() {
+    var text = "";
+    if (document.getSelection) {
+        text = document.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
